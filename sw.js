976e6a0003cf3d1634162e430d813115ba0d5228
scheurermarc77-spec <scheurermarc77@gmail.com>
2026-09-24 21:52:18 +0200

    App-Cache für Tabellenkorrektur erneuern

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='liga4daily-v73';
+const CACHE='liga4daily-v74';
 const ASSETS=[
   './','index.html','styles.css','stats-flip.css','theme-yellowblack.css','bottom-update.css',
   'team-photo-source.css','match-tip.css','app-menu.css','mood-meter.css','scorer-hearts.css',
